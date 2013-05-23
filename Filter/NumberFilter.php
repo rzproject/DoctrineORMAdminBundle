3f1b9968423de07fba2928e9e2cb71e2ac2c6a67
Mell Zamora <mellzamora@outlook.com>
2013-05-23 09:16:28 +0800

    added license

--- Filter/NumberFilter.php
+++ Filter/NumberFilter.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the RzDoctrineORMAdminBundle package.
+ *
+ * (c) mell m. zamora <mell@rzproject.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Rz\DoctrineORMAdminBundle\Filter;
 
 use Sonata\DoctrineORMAdminBundle\Filter\NumberFilter as BaseNumberFilter;
